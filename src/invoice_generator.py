601f503ad12cd5f285b96a1581d641d824c78562
hwalton <harveywalton.hw@gmail.com>
2023-12-01 22:48:16 +0000

    invoies render from a list

--- src/invoice_generator.py
+++ src/invoice_generator.py
@@ -22,32 +22,48 @@ env = Environment(loader=FileSystemLoader('../invoices'))
 #print(f"os.getcwd: {os.getcwd()}")
 template = env.get_template('invoices_template.tex')
 
+invoice_list = []
+
 # Create a list of InvoiceData objects
 invoice_data = InvoiceData(
-    po_num='XXXX',
+    po_num='XXX1',
     description='Desvsnfd',
     unit_price=40,
     qty=1,
 )
 
-# Render the template with data from the DataFrame
-rendered_tex = template.render( po_num=invoice_data.po_num,
-                                description=invoice_data.description,
-                                unit_price=invoice_data.unit_price,
-                                qty=invoice_data.qty,
-                                amount=invoice_data.amount,
-                                total_amount=invoice_data.total_amount
+invoice_list.append(invoice_data)
+
+
+# Create a list of InvoiceData objects
+invoice_data = InvoiceData(
+    po_num='XXX2',
+    description='Desvsnfd',
+    unit_price=40,
+    qty=1,
 )
 
-# Write the rendered LaTeX to a file
-tex = f'../invoices/invoice_{invoice_data.po_num}'
-tex_tex = f'{tex}.tex'
-with open(tex_tex, 'w') as f:
-    f.write(rendered_tex)
+invoice_list.append(invoice_data)
 
-# Compile the LaTeX file to PDF (ensure pdflatex is in your PATH)
-tex_dir = os.path.dirname(os.path.abspath(tex_tex))
-subprocess.run(['pdflatex', os.path.basename(tex_tex)], cwd=tex_dir)
+for invoice_data in invoice_list:
+    # Render the template with data from the DataFrame
+    rendered_tex = template.render( po_num=invoice_data.po_num,
+                                    description=invoice_data.description,
+                                    unit_price=invoice_data.unit_price,
+                                    qty=invoice_data.qty,
+                                    amount=invoice_data.amount,
+                                    total_amount=invoice_data.total_amount
+    )
 
-for ext in ['log', 'aux']:
-    os.remove(f'{tex}.{ext}')
+    # Write the rendered LaTeX to a file
+    tex = f'../invoices/invoice_{invoice_data.po_num}'
+    tex_tex = f'{tex}.tex'
+    with open(tex_tex, 'w') as f:
+        f.write(rendered_tex)
+
+    # Compile the LaTeX file to PDF (ensure pdflatex is in your PATH)
+    tex_dir = os.path.dirname(os.path.abspath(tex_tex))
+    subprocess.run(['pdflatex', os.path.basename(tex_tex)], cwd=tex_dir)
+
+    for ext in ['log', 'aux']:
+        os.remove(f'{tex}.{ext}')
