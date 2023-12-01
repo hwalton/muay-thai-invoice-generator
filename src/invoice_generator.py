a884ef9d18d2eaf1d500188f4b2bc3dcdb72463f
hwalton <harveywalton.hw@gmail.com>
2023-12-01 22:32:45 +0000

    latex invoice formatted and inputs dummy data from python

--- src/invoice_generator.py
+++ src/invoice_generator.py
@@ -1,6 +1,52 @@
 import pandas as pd
+from jinja2 import Environment, FileSystemLoader
+import os
+import subprocess
+
+class InvoiceData:
+    def __init__(self, po_num, description, unit_price, qty):
+        self.po_num = po_num
+        self.description = description
+        self.unit_price = unit_price
+        self.qty = qty
+        self.amount = qty * unit_price
+        self.total_amount = self.amount
+
 
 # Load an Excel file into a pandas DataFrame
 df = pd.read_excel('../data/sessions.xlsx', sheet_name='Sessions')
+print(f'df: {df}')
 
-print(f"df: {df}")
+# Load the Jinja2 template
+env = Environment(loader=FileSystemLoader('../invoices'))
+#print(f"os.getcwd: {os.getcwd()}")
+template = env.get_template('invoices_template.tex')
+
+# Create a list of InvoiceData objects
+invoice_data = InvoiceData(
+    po_num='XXXX',
+    description='Desvsnfd',
+    unit_price=40,
+    qty=1,
+)
+
+# Render the template with data from the DataFrame
+rendered_tex = template.render( po_num=invoice_data.po_num,
+                                description=invoice_data.description,
+                                unit_price=invoice_data.unit_price,
+                                qty=invoice_data.qty,
+                                amount=invoice_data.amount,
+                                total_amount=invoice_data.total_amount
+)
+
+# Write the rendered LaTeX to a file
+tex = '../invoices/output.tex'
+with open(tex, 'w') as f:
+    f.write(rendered_tex)
+
+# Compile the LaTeX file to PDF (ensure pdflatex is in your PATH)
+tex_dir = os.path.dirname(os.path.abspath(tex))
+subprocess.run(['pdflatex', os.path.basename(tex)], cwd=tex_dir)
+
+for ext in ['log', 'aux']:
+    os.remove(f'../invoices/output.{ext}')
